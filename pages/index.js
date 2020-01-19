8500043eece01debb1a55c6480fefe151d29c6a7
Brian Beck <exogen@gmail.com>
2020-01-19 13:57:00 -0800

    Add notes in pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,18 @@
 
 function removedHttpsFunction() {
   console.log("confirm this was removed by searching for test-xxx-https");
+  // Note below, putting the check here does successfully exclude it!
+  // if (!process.browser) {
   const HttpsAgent = require("https").Agent;
   return new HttpsAgent();
+  // }
 }
 
 function testImports() {
+  // Even removing the conditional altogether and doing the DCE ourselves via
+  // const agent = undefined;
+  // so that there is no possible reference to `removedHttpsFunction` leaves
+  // the `https` import in the bundle.
   const agent = process.browser ? undefined : removedHttpsFunction();
   console.log(agent);
 }
