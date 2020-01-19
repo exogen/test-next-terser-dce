4b182568677b8f90ccb6930b0a189b767916159d
Brian Beck <exogen@gmail.com>
2020-01-19 13:49:18 -0800

    Confirmed moment also triggers it

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
-function removedFunction() {
-  console.log("confirm this was removed by searching for test-xxx-abc");
+// Why are these modules still included in the bundle if the only import
+// location is entirely removed?
+
+function removedHttpsFunction() {
+  console.log("confirm this was removed by searching for test-xxx-https");
   const HttpsAgent = require("https").Agent;
   return new HttpsAgent();
 }
 
-const agent = process.browser ? undefined : removedFunction();
+function testImports() {
+  const agent = process.browser ? undefined : removedHttpsFunction();
+  console.log(agent);
+}
+
+testImports();
 
 export default () => "Hello, world!";
